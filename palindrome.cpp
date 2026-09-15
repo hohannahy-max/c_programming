0a52ae04f63589a4e24661c328992a8316a88585
U-BSD\460392 <460392@SHS-H4W2NW3.beaverton.k12.or.us>
2026-09-15 12:39:46 -0700

    yay

---
+++ palindrome.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+
+int main(){
+  char str[80];
+  cin >> str;
+  cout << "It works!";
+
+  bool palindrome = false;
+
+  if (palindrome == true){
+    cout << "Palindrome";
+  }
+
+  return 0;
+}
