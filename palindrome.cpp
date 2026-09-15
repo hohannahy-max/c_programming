da8d937d674c9e61e9b028eedcf3acfc5f766ea8
U-BSD\460392 <460392@SHS-H4W2NW3.beaverton.k12.or.us>
2026-09-15 13:10:59 -0700

    pls work

--- palindrome.cpp
+++ palindrome.cpp
@@ -7,13 +7,23 @@ using namespace std;
 int main(){
   char str[80];
   cin >> str;
-  cout << "It works!";
+  for (int i =0, len = strlen(str); i <len; i++){
+    if (ispunct(str[]))
+      {
+	str.erase(i--,1);
+	len = strlen(len);
 
+      }
+
+
+  }
+  cout << str;
+  /*
   bool palindrome = false;
 
   if (palindrome == true){
     cout << "Palindrome";
   }
-
+  */
   return 0;
 }
