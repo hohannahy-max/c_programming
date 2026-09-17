90292bb0fcb6f8b8a62208cdd0eaad82c964f279
U-BSD\460392 <460392@SHS-H4W2NW3.beaverton.k12.or.us>
2026-09-17 13:05:18 -0700

    null character:(

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,29 +1,47 @@
 #include <iostream>
 #include <cstring>
+#include <cctype>
 using namespace std;
 
 
 
 int main(){
-  char str[80];
-  cin >> str;
+  char str[80]; // array to write in
+  char str2[80]; //array to write reverse in
+  cin.get(str,80);
+
+  
+
+    /*
+  //Get rid of spaces and punctuation and make same case
   for (int i =0, len = strlen(str); i <len; i++){
-    if (ispunct(str[]))
+
+  put in each character until  
+    if (isalpha(str[i]) == 0)
       {
 	str.erase(i--,1);
 	len = strlen(len);
 
       }
+     if (isspace(str[i]) == 1){
+
+
+     }
 
 
   }
-  cout << str;
-  /*
-  bool palindrome = false;
 
-  if (palindrome == true){
-    cout << "Palindrome";
-  }
+
+   strcpy(str2, str);
   */
+  cout << str << endl;
+
+  if (strcmp(str, str2) == 0){
+    cout << "Palindrome" << endl;
+  } else {
+    cout << "Not a palindrome" << endl;
+
+
+  }
   return 0;
 }
