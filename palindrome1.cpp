6b21393d128ca8a8562c70fcc95acf910f2955fa
hohannahy-max <ho.hannahy@gmail.com>
2026-09-18 20:11:01 -0700

    my head hurts

---
+++ palindrome1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+using namespace std;
+
+
+
+int main(){
+  char str[81]; // array to write in
+  char clean[81]; //array to write reverse in
+  char reverse[81];
+  cin.get(str,81);
+  
+
+  
+  //Get rid of spaces and punctuation and make same case
+  int len = 0;
+  for (int i =0;str[i] != '\0'; i++){
+ //put in each character until  
+    if (isalnum(str[i]))
+      {
+      clean[len] =  tolower(str[i]);
+      len++;
+
+      }
+
+
+     }
+clean[len] = '\0';
+
+for (int i = 0; i < len; i++){
+reverse[i] = clean[len - 1 -i];
+}
+reverse[len] = '\0';
+
+  if (strcmp(clean, reverse) == 0){
+    cout << "Palindrome" << endl;
+  } else {
+    cout << "Not a palindrome" << endl;
+
+
+  }
+  return 0;
+}
